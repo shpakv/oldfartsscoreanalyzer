bdf89f1b18d199c63a5c4963b3ff8788d98eafcf
z003pz4w <vyacheslav.shpak@siemens.com>
2025-10-29 12:12:27 +0000

    feat(teambuilder):  remove bot token

--- internal/telegram/bot.go
+++ internal/telegram/bot.go
@@ -5,7 +5,6 @@ import "oldfartscounter/internal/environment"
 const ChatID = "-1002150403113"
 const BotID = "-veryveryoldfartbot"
 const BotName = "OldFartsBot"
-const BotToken = "7890733898:AAF1Vak1FwUQm-aU1OCrm6svxSXVS9gnago"
 
 type Bot struct {
 	Id    string `json:"id"`
@@ -17,6 +16,6 @@ func NewBotFromEnv() *Bot {
 	return &Bot{
 		Id:    environment.GetVariable("TELEGRAM_BOT_ID", BotID),
 		Name:  environment.GetVariable("TELEGRAM_BOT_NAME", BotName),
-		Token: environment.GetVariable("TELEGRAM_BOT_TOKEN", BotToken),
+		Token: environment.GetVariable("TELEGRAM_BOT_TOKEN"),
 	}
 }
